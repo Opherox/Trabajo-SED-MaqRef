c921b438cc6761daa792cb8c2d16733ea730e870
Miguel Ángel Pascual Collar <61047659+Opherox@users.noreply.github.com>
2023-12-19 18:32:53 +0100

    Update Entidad Generador de secuencias.vhd

--- Maquina Refrescos Code/Visualizador/Generador de secuencias/Entidad Generador de secuencias.vhd
+++ Maquina Refrescos Code/Visualizador/Generador de secuencias/Entidad Generador de secuencias.vhd
@@ -1,35 +1,6 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 13.12.2023 10:58:58
--- Design Name: 
--- Module Name: gen_secuencias - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.NUMERIC_STD.ALL;
 
 entity gen_secuencias iS
     Generic(N:Positive:=8);
@@ -41,17 +12,34 @@ entity gen_secuencias iS
 end gen_secuencias;
 
 architecture Behavioral of gen_secuencias is
+
     signal sel_in: std_logic_vector(N-1 downto 0):=(0=>'0',OTHERS=>'1');
+    
 begin
+    
     process(clk,RESET)
+    variable i : integer := 0;
         begin
         if RESET = '1' then
-            sel_in<=(0=>'0',OTHERS=>'1');
+            sel_in <=(0=>'0',OTHERS=>'1'); --elemento 0 del vector a 0, el resto a 1
+            i := 0;
         elsif rising_edge(clk) then
-            if CE = '1' then
-                sel_in <= sel_in(N-2 downto 0) & sel_in(N-1);
+            if CE = '1' then --si se habilita el cambio
+                i := i+1;
+                if i > N-1 then --i mayor que N-1 empezar la secuencia de 0
+                    i := 0;
+                end if;
+                for j in 0 to N-1 loop  -- como es logica negada el elemento a seleccionar a 0 el resto a 1
+                    if (i=j) then
+                    sel_in(j) <= '0';
+                    else
+                    sel_in(j) <= '1';
+                    end if;
+                end loop; 
+                --sel_in <= sel_in(N-2 downto 0) & sel_in(N-1);
             end if;
         end if;
      end process;
      SEL <= sel_in;
-end Behavioral;
+     
+     end Behavioral;
