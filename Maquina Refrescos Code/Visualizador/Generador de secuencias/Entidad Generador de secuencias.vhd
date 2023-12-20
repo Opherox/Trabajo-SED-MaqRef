ad8909019fd8ad15e987c99f28c629f9840cded8
davidpintollorente <80397735+davidpintollorente@users.noreply.github.com>
2023-12-20 12:08:29 +0100

    Comentarios Generador de Secuencias

--- Maquina Refrescos Code/Visualizador/Generador de secuencias/Entidad Generador de secuencias.vhd
+++ Maquina Refrescos Code/Visualizador/Generador de secuencias/Entidad Generador de secuencias.vhd
@@ -2,44 +2,27 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity gen_secuencias iS
-    Generic(N:Positive:=8);
+entity gen_secuencias is
+    Generic(N:Positive:=8); -- generico para numero de diplays ha utilizar
     Port(
-    CE: in std_logic;
-    clk: in std_logic;
-    RESET: in std_logic;
-    SEL: out std_logic_vector(N-1 downto 0));
+    CE: in std_logic -- entrada de habilitacon procedente del timer
+    clk: in std_logic; -- relog general
+    RESET: in std_logic; -- reset general
+    SEL: out std_logic_vector(N-1 downto 0)); -- salida de habilitacion de displays
 end gen_secuencias;
 
 architecture Behavioral of gen_secuencias is
-
-    signal sel_in: std_logic_vector(N-1 downto 0):=(0=>'0',OTHERS=>'1');
-    
+    signal sel_in: std_logic_vector(N-1 downto 0):=(0=>'0',OTHERS=>'1'); -- registros internos para la salida de habiliatcion de los displays 
 begin
-    
     process(clk,RESET)
-    --variable i : integer := 0;
         begin
-        if RESET = '1' then
-            sel_in <=(0=>'0',OTHERS=>'1'); --elemento 0 del vector a 0, el resto a 1
-            --i := 0;
-        elsif rising_edge(clk) then
-            if CE = '1' then --si se habilita el cambio
-                --i := i+1;
-                --if i > N-1 then --i mayor que N-1 empezar la secuencia de 0
-                    --i := 0;
-                --end if;
-                --for j in 0 to N-1 loop  -- como es logica negada el elemento a seleccionar a 0 el resto a 1
-                    --if (i=j) then
-                    --sel_in(j) <= '0';
-                    --else
-                    --sel_in(j) <= '1';
-                    --end if;
-                --end loop; 
-               sel_in <= sel_in(N-2 downto 0) & sel_in(N-1);
+        if RESET = '1' then -- si se activa el reset general 
+            sel_in <=(0=>'0',OTHERS=>'1'); -- habilita solo uno de los displays
+        elsif rising_edge(clk) then -- cuando haya un flanco positivo de reloj
+            if CE = '1' then -- se habilita el dispositivo
+               sel_in <= sel_in(N-2 downto 0) & sel_in(N-1); -- rotacion de un bit hacia la izquierda
             end if;
         end if;
      end process;
-     SEL <= sel_in;
-     
+     SEL <= sel_in; -- asignacion de la salida con la señal interna     
 end Behavioral;
