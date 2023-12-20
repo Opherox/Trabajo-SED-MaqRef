80cd76f84dd110db96ffbe027c20c6f09d96c8a7
davidpintollorente <80397735+davidpintollorente@users.noreply.github.com>
2023-12-20 11:50:51 +0100

    Comentarios de Gestor de Dinero

--- Maquina Refrescos Code/Gestor Dinero/Gestor Dinero.vhdl
+++ Maquina Refrescos Code/Gestor Dinero/Gestor Dinero.vhdl
@@ -4,23 +4,23 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity Gestor_Dinero is
     Port(
-        cent10 : in std_logic;
-        cent20 : in std_logic;
-        cent50 : in std_logic;
-        cent100 : in std_logic;
-        clk : in std_logic;
-        reset : in std_logic;
-        Reset_D : in std_logic;
-        precio : in integer;
-        falta_dinero : out std_logic;
-        sobra_dinero : out std_logic;
-        dinero_justo : out std_logic;
-        dinero_act : out integer);
+        cent10 : in std_logic; -- entrada de 10 centimos
+        cent20 : in std_logic; -- entrada de 20 centimos
+        cent50 : in std_logic; -- entrada de 50 centimos
+        cent100 : in std_logic; -- entrada de 100 centimos
+        clk : in std_logic; -- reloj general
+        reset : in std_logic; -- reset general
+        Reset_D : in std_logic; -- CE para desabilitar los dispositivos
+        precio : in integer; -- precio del producto
+        falta_dinero : out std_logic; -- señal de que falta dinero
+        sobra_dinero : out std_logic; -- señal de que sobra 
+        dinero_justo : out std_logic; -- señal de dinero justo
+        dinero_act : out integer); -- dinero actual introducido
 end Gestor_Dinero;
 
 architecture Estructural of Gestor_Dinero is
     
-    component comparador is
+    component comparador is -- declaracion del la entidad comparador
         Port(
         dinero_act : in integer;
         precio : in integer;
@@ -32,7 +32,7 @@ architecture Estructural of Gestor_Dinero is
         sobra_dinero : out std_logic);
      end component;
      
-     component contador is
+     component contador is -- declaracion de la entidad contador
         Port(
         cent10 : in std_logic;
         cent20 : in std_logic;
@@ -44,11 +44,11 @@ architecture Estructural of Gestor_Dinero is
         dinero : out integer); 
      end component;
      
-signal dinero_actual : integer := 0;
+signal dinero_actual : integer := 0; -- señal interna para conectar los componenetes: dinero actual introducido
      
 begin
 
- cont: contador
+ cont: contador -- mapeado de las señales del contador
     Port map(
     cent10=>cent10,
     cent20=>cent20,
@@ -59,7 +59,7 @@ begin
     clk=>clk,
     dinero=>dinero_actual);
     
- comp: comparador
+ comp: comparador -- mapeado de las señales del comparador
     Port map(
     dinero_act=>dinero_actual,
     precio=>precio,
@@ -70,6 +70,6 @@ begin
     dinero_justo=>dinero_justo,
     sobra_dinero=>sobra_dinero);
     
-    dinero_act <= dinero_actual;
+    dinero_act <= dinero_actual; -- se asigna la variable interna a la salida
     
 end Estructural;
