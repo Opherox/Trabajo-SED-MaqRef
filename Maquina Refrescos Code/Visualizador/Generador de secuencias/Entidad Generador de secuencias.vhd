9298d754377c6373d6f68502718654a25b2db519
Miguel Ángel Pascual Collar <61047659+Opherox@users.noreply.github.com>
2023-12-20 11:21:16 +0100

    Update Entidad Generador de secuencias.vhd

--- Maquina Refrescos Code/Visualizador/Generador de secuencias/Entidad Generador de secuencias.vhd
+++ Maquina Refrescos Code/Visualizador/Generador de secuencias/Entidad Generador de secuencias.vhd
@@ -18,25 +18,25 @@ architecture Behavioral of gen_secuencias is
 begin
     
     process(clk,RESET)
-    variable i : integer := 0;
+    --variable i : integer := 0;
         begin
         if RESET = '1' then
             sel_in <=(0=>'0',OTHERS=>'1'); --elemento 0 del vector a 0, el resto a 1
-            i := 0;
+            --i := 0;
         elsif rising_edge(clk) then
             if CE = '1' then --si se habilita el cambio
-                i := i+1;
-                if i > N-1 then --i mayor que N-1 empezar la secuencia de 0
-                    i := 0;
-                end if;
-                for j in 0 to N-1 loop  -- como es logica negada el elemento a seleccionar a 0 el resto a 1
-                    if (i=j) then
-                    sel_in(j) <= '0';
-                    else
-                    sel_in(j) <= '1';
-                    end if;
-                end loop; 
-                --sel_in <= sel_in(N-2 downto 0) & sel_in(N-1);
+                --i := i+1;
+                --if i > N-1 then --i mayor que N-1 empezar la secuencia de 0
+                    --i := 0;
+                --end if;
+                --for j in 0 to N-1 loop  -- como es logica negada el elemento a seleccionar a 0 el resto a 1
+                    --if (i=j) then
+                    --sel_in(j) <= '0';
+                    --else
+                    --sel_in(j) <= '1';
+                    --end if;
+                --end loop; 
+               sel_in <= sel_in(N-2 downto 0) & sel_in(N-1);
             end if;
         end if;
      end process;
